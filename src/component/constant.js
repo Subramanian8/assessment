66105a44fbea8725e1d83326abd501afba504a6c
subramanian.s <subramanian.s@inxsasia.com>
2021-11-25 21:58:09 +0530

    initial commit

---
+++ src/component/constant.js
@@ -0,0 +1,302 @@
+export const dataList = [
+   {
+      "Name": "Item 1",
+      "Price": 10,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 2",
+      "Price": 100,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 3",
+      "Price": 30,
+      "Weight": 300
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 4",
+      "Price": 20,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 5",
+      "Price": 30,
+      "Weight": 250
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 6",
+      "Price": 40,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 7",
+      "Price": 200,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 8",
+      "Price": 120,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 9",
+      "Price": 130,
+      "Weight": 790
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 10",
+      "Price": 20,
+      "Weight": 100
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 11",
+      "Price": 10,
+      "Weight": 340
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 12",
+      "Price": 4,
+      "Weight": 800
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 13",
+      "Price": 5,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 14",
+      "Price": 240,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 15",
+      "Price": 123,
+      "Weight": 700
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 16",
+      "Price": 245,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 17",
+      "Price": 230,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 18",
+      "Price": 110,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 19",
+      "Price": 45,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 20",
+      "Price": 67,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 21",
+      "Price": 88,
+      "Weight": 300
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 22",
+      "Price": 10,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 23",
+      "Price": 17,
+      "Weight": 250
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 24",
+      "Price": 19,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 25",
+      "Price": 89,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 26",
+      "Price": 45,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 27",
+      "Price": 99,
+      "Weight": 790
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 28",
+      "Price": 125,
+      "Weight": 100
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 29",
+      "Price": 198,
+      "Weight": 340
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 30",
+      "Price": 220,
+      "Weight": 800
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 31",
+      "Price": 249,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 32",
+      "Price": 230,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 33",
+      "Price": 190,
+      "Weight": 700
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 34",
+      "Price": 45,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 35",
+      "Price": 12,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 36",
+      "Price": 5,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 37",
+      "Price": 2,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 38",
+      "Price": 90,
+      "Weight": 20
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 39",
+      "Price": 12,
+      "Weight": 300
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 40",
+      "Price": 167,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 41",
+      "Price": 12,
+      "Weight": 250
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 42",
+      "Price": 8,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 43",
+      "Price": 2,
+      "Weight": 10
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 44",
+      "Price": 9,
+      "Weight": 500
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 45",
+      "Price": 210,
+      "Weight": 790
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 46",
+      "Price": 167,
+      "Weight": 100
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 47",
+      "Price": 23,
+      "Weight": 340
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 48",
+      "Price": 190,
+      "Weight": 800
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 49",
+      "Price": 199,
+      "Weight": 200
+      , "isChecked": false
+   },
+   {
+      "Name": "Item 50",
+      "Price": 12,
+      "Weight": 20
+      , "isChecked": false
+   }
+]
---
+++ src/component/list-header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodosList = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Weight</th>
+                <th>Select</th>
+            </tr>
+        </thead>
+    )
+}
+export default TodosList;
